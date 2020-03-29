12432b45726e1254b128206baecfe390e46f825a
Tung Vu <vutung.ct@gmail.com>
2020-03-29 23:17:03 +0700

    Ignore remarks while compilling

--- acpica_dump/build_acpio_override.sh
+++ acpica_dump/build_acpio_override.sh
@@ -5,7 +5,7 @@ set -e
 CUR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd $CUR_DIR
 
-iasl -tc dsdt.dsl 2>&1 | tee compile.log
+iasl -vr -tc dsdt.dsl 2>&1 | tee compile.log
 cp dsdt.aml ./kernel/firmware/acpi/
 find kernel | cpio -H newc --create > acpi_override.cpio
 sudo cp acpi_override.cpio /boot/efi/
